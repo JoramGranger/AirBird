eb3dc280b3299fd5097e099d3eeef0b150f7e68e
joram <joramgranger@outlook.com>
2024-09-22 02:40:28 +0300

    fonts

--- app/splash/index.tsx
+++ app/splash/index.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { Text, View, Image, StyleSheet } from 'react-native';
 import Logo from '../../assets/airbird.svg';
+import colors from "../../theme/colors";
+import { useFonts } from "expo-font";
+import { Montserrat_400Regular, Montserrat_700Bold_Italic } from "@expo-google-fonts/montserrat";
 
 const SplashScreen = () => {
+
+  const [fontsLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_700Bold_Italic,
+  });
+
+  if (!fontsLoaded) {return <Text>App Loading</Text>}
   return(
     <View style={styles.container}>
         <Logo width={200} height={200} />
-      <Text>AirBird</Text>
+      <Text style={styles.text}>Air-Bird</Text>
     </View>
   );
 }
@@ -16,12 +26,17 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#F0F0F0',
+        backgroundColor: colors.background
     },
     logo: {
         width: 200,
         height: 200,
     },
+    text: {
+      fontSize: 60,
+      color: colors.secondary,
+      fontFamily: 'Montserrat_700Bold_Italic',
+    }
 });
 
 export default SplashScreen;
---
+++ theme/colors.ts
@@ -0,0 +1,14 @@
+const colors = {
+    primary: '#f1f500',        // yellow   
+    secondary: '#0e235d',      // dark navy
+    background: '#ecf0f1',     // Light gray
+    textPrimary: '#2c3e50',    // Dark gray
+    textSecondary: '#95a5a6',  // Medium gray
+    white: '#ffffff',          // White
+    black: '#000000',          // Black
+    warning: '#f39c12',        // Orange
+    success: '#27ae60',        // Success Green
+    danger: '#c0392b',         // Error Red
+}
+
+export default colors;
